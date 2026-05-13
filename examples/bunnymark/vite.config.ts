71f556322d218ac4593fbc393e7bc63192a3fe3a
jgranick <jgranick@users.noreply.github.com>
2026-05-13 13:33:44 -0700

    examples: Remove bunnymark for now

--- examples/bunnymark/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig, mergeConfig } from 'vite';
-
-import { createBaseConfig } from '../../vite.config.base';
-
-export default defineConfig((env) => {
-  return mergeConfig(createBaseConfig(env.mode), {
-    root: __dirname,
-  });
-});
